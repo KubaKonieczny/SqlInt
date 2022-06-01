01fd383b484a1104806d8479f40b294c77514e94
Kuba <konieczny.jakub@onet.pl>
2022-06-01 17:06:03 +0200

    query_exec niedokonczony szablon

--- query_exec.py
+++ query_exec.py
@@ -9,13 +9,24 @@ class Operations(object):
 
     def query_exec(self, name, columns, tables, expressions, order, where):
 
-        if name == "select" and expressions == None:
+        if name == "select" and expressions is None:
             df = pd.read_csv("Tables/"+tables)
             df = df[columns]
+
             #df.sort_values(order, in_place=True)
             print(df.head())
-        elif name == "select" and expressions != None:
+        elif name == "select" and expressions is not None :
             print(expressions)
+
+        elif name == 'update':
+            df = pd.read_csv("Tables/" + tables)
+            df.loc[:, columns] = expressions
+        elif name == 'insert':
+            df = pd.read_csv("Tables/" + tables)
+            df.loc[len(df.index)] = [expressions]
+        elif name == 'delete':
+            df = pd.read_csv("Tables/" + tables)
+            df.drop()
 
 
 
