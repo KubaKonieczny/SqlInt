55d2e9c205902ce7754d59c55fbf9eca934cca6d
maja202 <maja.swierk18@gmail.com>
2022-06-04 16:20:17 +0200

    Change in executing queries: SELECT works with *, WHERE clause works with one-word values. Change in parsing: assignment to p[0].

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 from query_exec import Operations
+
 op = Operations()
 reserved = {
     'SELECT': 'SELECT',
@@ -33,124 +34,156 @@ tokens = ['DOT', 'COMMA', 'LPAR', 'RPAR', 'INTEGER', 'DOUBLE', 'TEXT', 'PUNCTUAT
 
 literals = ['+', '-', '*', '/', '=', '<', '>', '^']
 
+
 # DEFINE TOKENS
 
 def t_SELECT(t):
     r'[Ss][Ee][Le][Ee][Cc][Tt]'
     return t
 
+
 def t_UPDATE(t):
     r'[Uu][Pp][Dd][Aa][Tt][Ee]'
     return t
+
 
 def t_INSERT(t):
     r'[Ii][Nn][Ss][Ee][Rr][Tt]'
     return t
 
+
 def t_DELETE(t):
     r'[Dd][Ee][Ll][Ee][Tt][Ee]'
     return t
+
 
 def t_FROM(t):
     r'[Ff][Rr][Oo][Mm]'
     return t
 
+
 def t_WHERE(t):
     r'[Ww][Hh][Ee][Rr][Ee]'
     return t
+
 
 def t_ORDER(t):
     r'[Oo][Rr][Dd][Ee][Rr]'
     return t
 
+
 def t_BY(t):
     r'[Bb][Yy]'
     return t
+
 
 def t_AND(t):
     r'[Aa][Nn][Dd]'
     return t
 
+
 def t_OR(t):
     r'[Oo][Rr]'
     return t
+
 
 def t_NOT(t):
     r'[Nn][Oo][Tt]'
     return t
 
+
 def t_AVG(t):
     r'[Aa][Vv][Gg]'
     return t
+
 
 def t_BETWEEN(t):
     r'[Bb][Ee][Tt][Ww][Ee][Ee][Nn]'
     return t
 
+
 def t_IN(t):
     r'[Ii][Nn]'
     return t
+
 
 def t_SUM(t):
     r'[Ss][Uu][Mm]'
     return t
 
+
 def t_MAX(t):
     r'[Mm][Aa][Xx]'
     return t
+
 
 def t_MIN(t):
     r'[Mm][Ii][Nn]'
     return t
 
+
 def t_COUNT(t):
     r'[Cc][Oo][Uu][Nn][Tt]'
     return t
 
+
 def t_AS(t):
     r'[Aa][Ss]'
     return t
+
 
 def t_DOUBLE(t):
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
 
+
 def t_INTEGER(t):
     r'\d+'
     return t
+
 
 def t_LPAR(t):
     r'\('
     return t
 
+
 def t_RPAR(t):
     r'\)'
     return t
+
 
 def t_COMMA(t):
     r'\,'
     return t
 
+
 def t_DOT(t):
     r'\.'
     return t
 
+
 def t_PUNCTUATION(t):
     r'\''
     return t
+
+
 def t_SET(t):
     r'[Ss][Ee][Tt]'
     return t
+
+
 def t_VALUES(t):
     r'[Vv][Aa][Ll][Uu][Ee][Ss]'
     return t
 
+
 def t_TEXT(t):
-    r'[A-Za-z]+|[a-zA-Z_][a-zA-Z0-9_]*|[A-Z]*\.[A-Z]$'
+    r'[\w]+|[\w\s]+'
     return t
 
-t_ignore  = ' \t'
+
+t_ignore = ' \t'
 
 
 # Error handling rule
@@ -158,9 +191,9 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 # Build the lexer
 lexer = lex.lex(debug=1)
-
 
 lexer.lexliterals
 
@@ -172,8 +205,9 @@ lexer.input(data)
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
+        break  # No more input
     print(tok)
+
 
 # PARSING
 def p_query(p):
@@ -182,16 +216,22 @@ def p_query(p):
             | insert
             | delete'''
 
+
 def p_update(p):
     '''update : UPDATE table SET column '=' expression
                 | UPDATE table SET column '=' expression WHERE conlist
                 '''
+
+
 def p_insert(p):
     '''insert : INSERT table VALUES LPAR expressions RPAR
                 | INSERT table LPAR columns RPAR VALUES LPAR expressions RPAR'''
+
+
 def p_delete(p):
     '''delete : DELETE FROM tables
                 | DELETE FROM tables WHERE conlist'''
+
 
 def p_select(p):
     ''' select : SELECT columns FROM tables WHERE conlist ORDER BY columns
@@ -200,9 +240,30 @@ def p_select(p):
                 | SELECT columns FROM tables
                 | SELECT expression
                 | SELECT expression FROM tables'''
-    c = p[2]
-    t = p[4]
-    op.query_exec("select", c, t, None, None, None)
+
+    c = None
+    t = None
+    o = None
+    e = None
+    w = None
+
+    if len(p) == 9 or len(p) == 7:
+        w = p[6]
+
+    if len(p) == 9:
+        o = p[8]
+    elif len(p) == 7:
+        o = p[6]
+
+    if len(p) == 3:
+        e = p[2]
+    else:
+        c = p[2]
+        t = p[4]
+
+    op.query_exec("select", c, t, e, o, w)
+
+
 def p_columns(p):
     ''' columns : columns COMMA column
                 | column
@@ -215,7 +276,8 @@ def p_columns(p):
             col.append(i)
         col.append(p[3])
     p[0] = col
-    
+
+
 def p_column(p):
     '''column :  '*'
                 | TEXT
@@ -225,7 +287,8 @@ def p_column(p):
         p[0] = p[1]
     else:
         p[0] = p[3]
-        
+
+
 def p_aggregate(p):
     ''' aggregate : SUM LPAR TEXT RPAR
                     | AVG LPAR TEXT RPAR
@@ -233,7 +296,8 @@ def p_aggregate(p):
                     | MIN LPAR TEXT RPAR
                     | COUNT LPAR TEXT RPAR '''
 
-def p_tabeles(p):
+
+def p_tables(p):
     '''tables : table
             | tables COMMA table'''
 
@@ -246,10 +310,10 @@ def p_tabeles(p):
         col.append(p[3])
     p[0] = col
 
+
 def p_table(p):
     ''' table : TEXT
             | TEXT AS TEXT
-
             '''
     p[0] = p[1]
 
@@ -262,14 +326,22 @@ def p_conlist(p):
                 | TEXT BETWEEN number AND number
                 | TEXT IN LPAR select RPAR
                  '''
+    p[0] = p[1]
 
 
 def p_value(p):
     ''' value :  number
                 | aggregate
-                |  PUNCTUATION TEXT PUNCTUATION  '''
+                | PUNCTUATION TEXT PUNCTUATION  '''
+    if len(p) == 4:
+        values = []
+        for val in p[1:]:
+            values.append(val)
 
-    p[0] = p[1]
+        p[0] = values
+    else:
+        p[0] = p[1]
+
 
 def p_condition(p):
     ''' condition : column '>' value
@@ -279,14 +351,19 @@ def p_condition(p):
                   | column '<' column
                   | column '=' column
                    '''
+    cond = []
+    for c in p[1:]:
+        cond.append(c)
 
+    p[0] = cond
 
 def p_number(p):
     ''' number : INTEGER
                 | DOUBLE '''
     p[0] = p[1]
 
-def  p_expressions(p):
+
+def p_expressions(p):
     ''' expressions : expressions COMMA expression
                 | expression'''
     expr = []
@@ -297,6 +374,8 @@ def  p_expressions(p):
             expr.append(i)
         expr.append(p[3])
     p[0] = expr
+
+
 def p_expression(p):
     ''' expression : expression '+' value
                    | expression '-' value
@@ -310,17 +389,18 @@ def p_expression(p):
             p[0] = p[1] - p[3]
 
 
-
 def p_error(p):
     print("Syntax error in input!")
 
     # Build the parser
+
+
 parser = yacc.yacc(debug=1)
 
 print(parser)
-#parser.parse(data, debug=1)
-#for x in parser.token()
-#print(parser.token())
+# parser.parse(data, debug=1)
+# for x in parser.token()
+# print(parser.token())
 while True:
     try:
         s = input("Enter query \n")
@@ -331,4 +411,3 @@ while True:
     result = parser.parse(s, debug=1)
 
     print(result)
-    
--- query_exec.py
+++ query_exec.py
@@ -9,16 +9,27 @@ class Operations(object):
 
     def query_exec(self, name, columns, tables, expressions, order, where):
 
-        if name == "select" and expressions is None:
-            df = pd.read_csv("Tables/"+tables[0])
-            print(columns)
-            print(df.columns)
-            df = df[columns]
+        if name == "select":
+            if expressions is not None:
+                print(expressions)
+            elif columns == ['*']:
+                df = pd.read_csv("Tables/" + tables[0])
+                print(df.head())
+            elif expressions is None and where is None and order is None:
+                df = pd.read_csv("Tables/" + tables[0])
+                print(columns)
+                print(df.columns)
+                df = df[columns]
 
-            #df.sort_values(order, in_place=True)
-            print(df.head())
-        elif name == "select" and expressions is not None :
-            print(expressions)
+                # df.sort_values(order, in_place=True)
+                print(df.head())
+            elif where is not None:
+                df = pd.read_csv("Tables/" + tables[0])
+                # print(where[0])
+                # print(where[2][1])
+                df = df[df[where[0]] == where[2][1]]
+                print(df)
+
 
         elif name == 'update':
             df = pd.read_csv("Tables/" + tables)
